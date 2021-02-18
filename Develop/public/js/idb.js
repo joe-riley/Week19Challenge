42e9849539e3f36481317d9b489eb425660c96f6
Joe Riley <joeyriley@gmail.com>
2021-02-18 12:38:03 -0700

    Save to Mongo after online

---
+++ Develop/public/js/idb.js
@@ -0,0 +1,67 @@
+let db;
+
+const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = e => {
+  db = e.target.result;
+  db.createObjectStore('transactions', { autoIncrement: true });
+};
+
+request.onsuccess = e => {
+  db = e.target.result;
+
+  if(navigator.onLine) {
+
+  }
+};
+
+request.onerror = e => {
+  console.log(e.target.errorCode, e.target.errorMessage);
+};
+
+const saveRecord = record => {
+  const transaction = db.transaction(['transactions'], 'readwrite');
+
+  const budgetObjectStore = transaction.objectStore('transactions');
+
+  budgetObjectStore.add(record);
+}
+
+const uploadRecord = () => {
+  const transaction = db.transaction(['transactions'], 'readwrite');
+
+  const budgetObjectStore = transaction.objectStore('transactions');
+
+  const getAll = budgetObjectStore.getAll();
+
+  getAll.onsuccess = async () => {
+    if (getAll.result.length > 0) {
+      try {
+        const response = await fetch('/api/transaction', {
+          method: 'POST',
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+          } 
+        });
+
+        if (response.json().message) {
+          throw new Error(response);
+        }
+
+        // Clear the object store now
+        const trans = db.transaction(['transactions'], 'readwrite');
+        const oldObjectStore = trans.objectStore('transactions');
+        oldObjectStore.clear();
+
+        console.log('All offline transactions have been uploaded.');
+
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+};
+
+window.addEventListener('online', uploadRecord);
